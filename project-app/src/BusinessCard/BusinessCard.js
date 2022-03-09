d9b9abba27f655d0f5a076bb6b64f492573a4bfd
Elijah Ohiwerei <elijahohimairei@gmail.com>
2022-03-09 14:52:08 +0100

    second project

--- project-app/src/BusinessCard/BusinessCard.js
+++ project-app/src/BusinessCard/BusinessCard.js
@@ -4,18 +4,24 @@ import profilepic from './profilepic.jpg'
 import mail from './mail.png'
 import Footer from './Footer/Footer'
 
-
-
 const businessCard = () => {
+
+    const style = {
+        color: "#F3BF99",
+        fontSize: "12.8px"
+    }
+
     return (
         <div className="container">
-            <img src={profilepic} alt="profilepic" />
+            <img className="profilepic" src={profilepic} alt="profilepic" />
             <h1>Elijah Ohiwerei</h1>
-            <p>Frontend Developer</p>
+            <p style={style}>Frontend Developer</p>
             <a href="https://elijahohiwerei.netlify.app">elijahO.website</a>
-            <img src={mail} alt="mail" />
-            <p>Email</p>
-            <div>
+            <div className="email_container">
+                <img src={mail} alt="mail" />
+                <p><a href="mailto:elijahohiwerei@gmail.com">Email</a></p>
+            </div>
+            <div className="about_container">
                 <h3>About</h3>
                 <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
                 <h3>Interests</h3>
