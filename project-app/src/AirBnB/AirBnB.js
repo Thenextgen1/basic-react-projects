2859c750676edb25075660378f28ec6c0771b1e2
Elijah Ohiwerei <elijahohimairei@gmail.com>
2022-03-19 16:24:55 +0100

    refactoring airbnb clone

--- project-app/src/AirBnB/AirBnB.js
+++ project-app/src/AirBnB/AirBnB.js
@@ -3,14 +3,38 @@ import './AirBnB.css'
 import Navbar from './Navbar/Navbar'
 import Hero from './Hero/Hero'
 import Card from './Card/Card'
-
+import Athlete from './Card/image 12.png'
+import Wed from './Card/wedding-photography 1.png'
+import Bike from './Card/mountain-bike 1.png'
 
 const airBnB = () => {
     return (
         <div className="airbnb_container">
             <Navbar />
             <Hero />
-            <Card />
+            <div className="card-container">
+                <Card
+                    heading="SOLD OUT"
+                    img={Athlete}
+                    rating={5.0}
+                    content="Life lessons with Katie Zaferes"
+                    rate="From $136 / person"
+                />
+                <Card
+                    heading="ONLINE"
+                    img={Wed}
+                    rating="5.0"
+                    content="Learn wedding Photography"
+                    rate="From $136 / person"
+                />
+                <Card
+                    heading="ONLINE"
+                    rating="5.0"
+                    content="Learn wedding Photography"
+                    rate="From $136 / person"
+                    img={Bike}
+                />
+            </div>
         </div>
     )
 }
--- project-app/src/AirBnB/Card/Card.js
+++ project-app/src/AirBnB/Card/Card.js
@@ -1,32 +1,41 @@
 import React from 'react'
 import './Card.css'
-import Athlete from './image 12.png'
-import Wed from './wedding-photography 1.png'
-import Bike from './mountain-bike 1.png'
 
 
-const card = () => {
+//you can either put props in the card parameters, then give add the object by calling props val
+const card = (props) => {
+
     return (
         <div className="card-container">
             <div>
-                <h3>SOLD OUT</h3>
-                <img src={Athlete} alt="Athlete" />
-                <p>5.0 <span>(6)<span>.</span> USA</span></p>
-                <p>Life lessons with Katie Zaferes</p>
-                <p>From $136 / person</p>
+                {
+                    props.heading && < h3 > {props.heading}</h3>
+                }
+                <img src={props.img} alt="Athlete" />
+                <p>{props.rating} <span>(6)<span>.</span> USA</span></p>
+                <p>{props.content}</p>
+                <p>{props.rate}</p>
             </div>
-            <div>
-                <h3 className='online'>ONLINE</h3>
-                <img src={Wed} alt="Wedding" />
-                <p>5.0 <span>(30)<span>.</span> USA</span></p>
-                <p>Learn wedding photography</p>
-                <p>From $125 / person</p>
-            </div>
-            <div className='bike-container'>
-                <img src={Bike} alt="Bike" />
-            </div>
-        </div>
+        </div >
     )
 }
+// Destructuring props
+
+// const card = ({heading, img, rating, content, rate}) => {
+
+//     return (
+//         <div className="card-container">
+//             <div>
+//                 {
+//                     heading && < h3 > {heading}</h3>
+//                 }
+//                 <img src={img} alt="Athlete" />
+//                 <p>{rating} <span>(6)<span>.</span> USA</span></p>
+//                 <p>{content}</p>
+//                 <p>{rate}</p>
+//             </div>
+//         </div >
+//     )
+// }
 
 export default card
